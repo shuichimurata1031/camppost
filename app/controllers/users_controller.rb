b3277a3ed83a7cf811b4845f946cae2754fe2cf2
murashi <murashi.camp@gmail.com>
2020-12-12 17:12:23 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
+#users#index では、User.order(id: :desc) で id の降順にユーザの一覧を取得しています。
+#また、ページネーションを適用させるために .page(params[:page]) を付けています。
+#さらに、per(25)、最大で25件取得できます。
 
   def show
     @user = User.find(params[:id])
@@ -12,5 +15,20 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
